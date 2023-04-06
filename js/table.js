d671bfec13c88c5aef2832896b14d96db6204bed
Dominik Meduna <medunadominik@gmail.com>
2023-04-06 21:46:26 +0200

    -Added comments -Added form reset for sub_button -Wrapped form in <form> -Code refactoring

--- js/table.js
+++ js/table.js
@@ -2,34 +2,39 @@ const sub_button = document.getElementById("create");
 client_arr = [];
 
 function read() {
+  //read data from form
   let first_name = document.getElementById("fr_na_txt").value;
   let last_name = document.getElementById("ls_na_txt").value;
   let age = document.getElementById("age").value;
   let phone_number = document.getElementById("phone_txt").value;
+  //create instance name
   let name = first_name + "_" + last_name;
-
+  //create new "client" instance and push it to client array
   name = new Client(first_name, last_name, age, phone_number);
   client_arr.push(name);
-
 }
 
 function renderDataInTheTable(client_arr) {
-    const mytable = document.getElementById("tbody");
-   document.getElementById("tbody").innerHTML = "";
-
-    client_arr.forEach(client => {
-        let newRow = document.createElement("tr");
-        Object.values(client).forEach((value)=> {
-            let cell = document.createElement("td");
-            cell.innerText = value
-            newRow.appendChild(cell)
-        })
-        mytable.appendChild(newRow)
-    })
+  //clear client table body
+  const mytable = document.getElementById("tbody");
+  mytable.innerHTML = "";
+  //cycle through client array
+  client_arr.forEach((client) => {
+    let newRow = document.createElement("tr");
+    //cycle through client object data and add them to "td"
+    Object.values(client).forEach((value) => {
+      let cell = document.createElement("td");
+      cell.innerText = value;
+      newRow.appendChild(cell);
+    });
+    //append new row to the table
+    mytable.appendChild(newRow);
+  });
 }
 
+//read form and redraw table
 sub_button.addEventListener("click", () => {
-    read();
-    console.log(client_arr)
-    renderDataInTheTable(client_arr)
-})
+  read();
+  renderDataInTheTable(client_arr);
+  document.getElementById("Form").reset()
+});
